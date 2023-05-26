3af549be3e2fb8d02869ef3b7fbd7cccc4375312
ttltna <ttltnaaa@gmail.com>
2023-05-26 22:06:04 +0900

    당근마켓 클론코딩 : 모바일화면에서의 header부분 먼저  세팅중 다 하지못함

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "75rem": "75rem",
+      },
+      lineHeight: {
+        0: "0",
+        full: "100%",
+      },
+      colors: {
+        carrothead: "#00B493",
+        carrot: "#FF7E36",
+        menu: "#212124",
+      },
+      strokeWidth: {
+        6: "6px",
+      },
+      fontSize: {
+        1.625: "1.625rem",
+      },
+    },
   },
   plugins: [],
 };
