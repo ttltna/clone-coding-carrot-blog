4234444c1d51478e7392be9f5753549ccc9ac02e
ttltna <ttltnaaa@gmail.com>
2023-05-30 22:50:06 +0900

    당근마켓 클론코딩 : header부분 sticky로 고정 완료 후 main 진행중

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,27 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      margin: {
+        "88px": "88px",
+        "100px": "100px",
+      },
+      padding: {
+        56.25: "56.25%",
+      },
+      minWidth: {
+        48: "48rem",
+      },
       maxWidth: {
-        "75rem": "75rem",
+        75: "75rem",
+      },
+      height: {
+        "68px": "68px",
+        "234px": "234px",
       },
       lineHeight: {
         0: "0",
+        2.4375: "2.4375rem",
+        4.5625: "4.5625rem",
         full: "100%",
       },
       colors: {
@@ -19,9 +35,15 @@ export default {
         6: "6px",
       },
       fontSize: {
+        1.6: "1.6rem",
         1.625: "1.625rem",
+        2.625: "2.625rem",
+        3.375: "3.375rem",
       },
     },
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
